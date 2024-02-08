56691696aa7a5b0afbdc5d269dc3e0ac526b5942
Carlos Sierra <sierracardozocarlosalexis@gmail.com>
2024-02-08 16:33:13 +0100

    plantilla spring

---
+++ Orion_Dreams/src/main/java/com/example/Orion_Dreams/OrionDreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Orion_Dreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionDreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrionDreamsApplication.class, args);
+	}
+
+}
---
+++ Orion_Dreams/src/test/java/com/example/Orion_Dreams/OrionDreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Orion_Dreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionDreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
